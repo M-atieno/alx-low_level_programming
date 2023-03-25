4be89ceb42a6a2c4efcc778562c7373d8170ebdf
m-atieno <matieno521@gmail.com>
2023-03-24 18:34:27 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,15 @@ int main(void)
 {
 	int m;
 
-	for (m = 0 ; m <= 10 ; m++)
-		if (m == 9)
-			putchar(m + '0');
-		else
+	for (m = 0 ; m < 10 ; m++)
+	{
+		putchar(m + '0');
+		if (m != 9)
 		{
-			putchar(m + '0');
 			putchar(',');
 			putchar(' ');
 		}
+	}
+	putchar('\n');
 	return (0);
 }
