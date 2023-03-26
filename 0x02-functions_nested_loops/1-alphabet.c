dad809ecf33c2c4d9084e6984edd6fc35dbc18d2
m-atieno <matieno521@gmail.com>
2023-03-25 18:12:03 -0700

    the alphabet game with prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - 'the function  prints lowercase alphabet'
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a' ;  j <= 'z' ; j++)
+		_putchar(j);
+	_putchar('\n');
+}
