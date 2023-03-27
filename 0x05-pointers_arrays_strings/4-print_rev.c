8adfeed0c70077b8300526e833817ba7512ece35
m-atieno <matieno521@gmail.com>
2023-03-27 15:58:41 -0700

    i do not fear computers reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
+/**
+ * print_rev - function to print string in reverse
+ *
+ * @s: function parameter
+ * Return: 0 when succesful
+ */
+
 void print_rev(char *s)
 {
+	int a;
+	int value = 0;
 
+	for (a = 0 ; s[a] != '\0'; a++)
+		value++;
+	for (a = value ; a >= 0 ; a--)
+		_putchar(s[a]);
+	_putchar('\n');
 }
