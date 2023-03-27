cf97e19348f4aca301844d354668b4206e092272
m-atieno <matieno521@gmail.com>
2023-03-27 15:46:22 -0700

    i do not fear computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,13 +3,14 @@
   * _puts - function that prints a string
   * @str: parameter for the function
   *
-  * Return: 0 when succesful
   */
 void _puts(char *str)
 {
 	int a;
 
 	for (a = 0 ; str[a] != '\0' ; a++)
+	{
 		_putchar(str[a]);
+	}
 	_putchar('\n');
 }
