dd0408119c19842044a73ba11b3b683bc892db4c
m-atieno <matieno521@gmail.com>
2023-03-27 12:31:18 -0700

    task 0 98 battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that resets the value of n
+ *
+ * @n: function parameter that replaces the n value
+ *
+ * Return: 0 (when succesful)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
