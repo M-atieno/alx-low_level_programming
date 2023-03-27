ed0cb7bba446aa8830e3763a22cddd7a5649655f
m-atieno <matieno521@gmail.com>
2023-03-27 05:18:45 -0700

    adding 2 intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that adds 2 intergers
+ *
+ * @i: function parameter 1
+ * @j: function parameter 2
+ *
+ * Return: a+b
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
