b0d7e088cfab09d394f9326d813038830ea1c415
m-atieno <matieno521@gmail.com>
2023-03-30 12:48:14 -0700

    strcmp task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string
+ * @s2: string
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	for (a = 0 ; s1[a] != '\0' || s2[a] != '\0' ;  a++)
+	{
+		if (s1[a] != s2[a])
+		{
+			if (s1[a] < s2[a])
+				return (s1[a] - s1[a]);
+			else if (s1[a] > s2[a])
+				return (s1[a] - s2[a]);
+		}
+		else
+			return (0);
+	}
+	return (0);
+}
